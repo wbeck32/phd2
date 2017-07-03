8dc246d16335ca9b952a4b81579015c295fddf98
Wendy Beck <webeck@gmail.com>
2017-07-02 22:19:07 -0700

    fixing a trainwreck!

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var gulp = require('gulp'),
   sourcemaps   = require('gulp-sourcemaps'),
   concat = require('gulp-concat'),
   watch = require("gulp-watch-sass"),
-  autoprefixer = require('autoprefixer');
+  autoprefixer = require('autoprefixer'),
+  less = require('gulp-less');
 
 
 gulp.task('default', function() {
@@ -26,7 +27,15 @@ gulp.task('sass', function () {
     .pipe(livereload());
 });
 
-gulp.task('start',['sass'],function(){
+// Compiles LESS > CSS
+gulp.task('build-less', function(){
+    return gulp.src('build/less/bootstrap.less')
+        .pipe(less())
+        .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('start',['sass', 'build-less'],function(){
   livereload.listen();
+  gulp.watch('build/less/*.less', ['sass']);
   gulp.watch('build/scss/*.scss', ['sass']);
 });
--- public/js/menu.js
+++
@@ -1,7 +0,0 @@
-$('ul li a.link').on('click', function() {
-  var m = $(this).attr('id');
-  var sectionId = '#' + m;
-  $('div.headerArea').addClass('responsiveHeader');
-  $('section.allsections').removeClass('visible').addClass('invisible');
-  $('section' + sectionId).removeClass('invisible').addClass('visible');
-});
