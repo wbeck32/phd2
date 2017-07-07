81175b4454c6bb3af3a2ab9af5c6583766ed927f
Wendy Beck <webeck@gmail.com>
2017-07-06 17:38:17 -0700

    partway through the css

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,17 @@
-$('ul li a.link').on('click', function() {
+$('ul li a.link').click(function() {
   var m = $(this).attr('id');
-  var sectionId = '#' + m;
-  $('section.allsections').removeClass('visible').addClass('invisible');
-  $('section' + sectionId).removeClass('invisible').addClass('visible');
+  console.log(m);
+  $('section').not('#'+m).css({'visibility' : 'hidden', 'height' : 0});
+  $('section#'+m).css('visibility', 'visible');
+  window.location.href = '#'+m;
 });
 
-
-
 $('.col-xs-3.col-md-4 a.link').click(function(){
-  var productTarget = $(this).attr('href');
+  var productTarget = $(this).attr('id');
   console.log(productTarget);
   window.location.href = '#'+productTarget;
   $('.responsiveHeader').css('visibility', 'hidden');
-  $('section.products').css('visibility', 'visible');
+  $('section#products').css('visibility', 'visible');
 });
 
 
