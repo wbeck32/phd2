32db5611358fb5df0f5336c1f655923543b316bf
Wendy Beck <webeck@gmail.com>
2017-07-09 16:43:30 -0700

    fixing product dropdown anchors

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,30 @@
 $('ul.nav.navbar-nav li a').click(function() {
   var m = $(this).attr('href');
-    console.log(m);
-  if (m == '#products') {
-    scrollToProducts();
-  } else if (m == '#moretop' || m == '#product1' || m == '#product2' || m == '#product3' || m == '#product4' ) {
+    if (m == '#moretop'){
       var dropdownChoice = $(this).find('ul li a').attr('href');
       $('section'+dropdownChoice).animate({
         scrollTop : 0
         }, 'slow', function(){
             // console.log('success!')
         });
-  } else if (m == '#home') {
+    } else if (m == '#product1' || m == '#product2' || m == '#product3' || m == '#product4') {
+      scrollToProductSection(m);
+    } else if (m == '#home') {
       $('#collapsedMenu.in').removeClass('in');
       $('section.allsections').css({'visibility' : 'hidden', 'display' : 'none'});
       $('html, body').animate({ scrollTop: 0 }, 'slow');
       $('div.pageBody div.responsiveHeader').css({'visibility' : 'visible', 'display' : 'inline-block'});
-  } else {
-    $('#collapsedMenu.in').removeClass('in');
-    $('.responsiveHeader').css({'visibility' : 'hidden', 'display' : 'none'});
-    $('section.allsections').not(m).css({'visibility' : 'hidden', 'display' : 'none'});
-    $('section'+m+'.allsections').css({'display': 'block','visibility' : 'visible'});
-    $('section'+m+'.allsections').animate({
-      scrollTop : 0
-      }, 'slow', function(){
-          // console.log('success!')
-      });
-  }
+    } else if (m !== undefined) {
+      $('#collapsedMenu.in').removeClass('in');
+      $('.responsiveHeader').css({'visibility' : 'hidden', 'display' : 'none'});
+      $('section.allsections').not(m).css({'visibility' : 'hidden', 'display' : 'none'});
+      $('section'+m+'.allsections').css({'display': 'block','visibility' : 'visible'});
+      $('section'+m+'.allsections').animate({
+        scrollTop : 0
+        }, 'slow', function(){
+            // console.log('success!')
+        });
+    }
 });
 
 $('.navbar-brand').click(function(){
@@ -66,6 +65,7 @@ function scrollToProducts () {
 }
 
 function scrollToProductSection(selector) {
+  $('.responsiveHeader').css({'visibility' : 'hidden', 'display' : 'none'});
   $('section#products.allsections').css({'visibility' : 'visible', 'display' : 'block'});
   $('div.product').not(selector).css({'visibility' : 'hidden', 'display' : 'none'});
   $('div.product').filter(selector).css({'visibility' : 'visible', 'display' : 'block'});
