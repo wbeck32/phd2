06fa7c54c47c7c3efac830d14fda332df4921268
Wendy Beck <webeck@gmail.com>
2017-07-15 19:09:32 -0700

    adding config files and gulp tests

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,16 @@ var gulp = require('gulp'),
   minifycss = require('gulp-clean-css'),
   uglify = require('gulp-uglify'),
   livereload = require('gulp-livereload'),
-  postcss      = require('gulp-postcss'),
-  sourcemaps   = require('gulp-sourcemaps'),
+  postcss = require('gulp-postcss'),
+  sourcemaps = require('gulp-sourcemaps'),
   concat = require('gulp-concat'),
   watch = require("gulp-watch-sass"),
   autoprefixer = require('autoprefixer'),
-  less = require('gulp-less');
+  mocha = require('gulp-mocha'),
+  less = require('gulp-less'),
+  jshint = require('gulp-jshint');
 
-
-gulp.task('default', function() {
+gulp.task('default', function () {
   gulp.start('start');
 });
 
@@ -20,7 +21,7 @@ gulp.task('sass', function () {
   return gulp.src('build/scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
+    .pipe(postcss([autoprefixer({ browsers: ['last 2 version'] })]))
     .pipe(sourcemaps.write())
     // .pipe(minifycss())
     .pipe(gulp.dest('public/css'))
@@ -28,16 +29,47 @@ gulp.task('sass', function () {
 });
 
 // Compiles LESS > CSS
-gulp.task('build-less', function(){
-    return gulp.src('build/less/bootstrap.less')
-        .pipe(less())
-        .pipe(gulp.dest('public/css'))
-        .pipe(livereload());
+gulp.task('build-less', function () {
+  return gulp.src('build/less/bootstrap.less')
+    .pipe(less())
+    .pipe(gulp.dest('public/css'))
+    .pipe(livereload());
 });
 
+gulp.task('buildJs', function () {
+  return gulp.src('build/js/app/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/js'))
+    .pipe(livereload());
+});
 
-gulp.task('start',['sass', 'build-less'],function(){
+gulp.task('updateHTML', function () {
+  return gulp.src('public/index.html')
+    .pipe(livereload());
+});
+
+gulp.task('test', function () {
+  return gulp.src('tests')
+    .pipe(mocha({
+      reporter: 'spec'
+    }))
+  once('error', function () {
+      process.exit(1);
+    })
+    .once('end', function () {
+      process.exit();
+    });
+});
+
+gulp.task('start', ['test', 'sass', 'build-less', 'buildJs'], function () {
   livereload.listen();
-  gulp.watch('build/less/*.less', ['sass', 'build-less']);
-  gulp.watch('build/scss/*.scss', ['sass', 'build-less']);
+  gulp.watch('build/less/*.less', ['build-less']);
+  gulp.watch('build/scss/*.scss', ['sass']);
+  gulp.watch('build/js/*.js', ['test', 'buildJs']);
+  gulp.watch('public/index.html', ['updateHTML']);
 });
