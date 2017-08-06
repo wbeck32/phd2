595cdb070547a495e076762fd50eb28c9aa1265d
Wendy Beck <webeck@gmail.com>
2017-08-05 17:26:04 -0700

    paring down the javascript based on streamlined http and css

--- build/js/app.js
+++ build/js/app.js
@@ -1,98 +1,30 @@
-function hideMail() {
-  console.log('obfuscation');
-  var u = 'user';
-  var arr = '@';
-  var d = 'domain';
-  var dot = '.';
-  var t = 'tldccc';
-  // document.write("<a href=" + "mail" + "to:" + u + arr + d + dot + t +
-  //   ">" + "Email (concatenation)" + "</a>" + "<br>");
-}
+/* eslint-disable */
 
-$('.footerTxt.col-md-4.col-sm-3 p').click(function() {
-// just send them to the contact form?
-});
-
-$('a').click(function() {
-  var m = $(this).attr('href');
-  if (m == '#moretop') {
-    var dropdownChoice = $(this)
-    .find('ul li a')
-    .attr('href');
-    $('section' + dropdownChoice).animate(
-      {
-        scrollTop: 0
-      },
-      'slow',
-      function() {
-        // console.log('success!')
-      }
-    );
-  } else if (
-    m == '#product1' ||
-    m == '#product2' ||
-    m == '#product3' ||
-    m == '#product4'
-  ) {
-    scrollToProductSection(m);
-  } else if (m == '#home') {
-    $('#collapsedMenu.in').removeClass('in');
-    $('section').css({ visibility: 'hidden', display: 'none' });
-    $('div.pageBody div.responsiveHeader').css({
-      visibility: 'visible',
-      display: 'block'
+$('.footerTxt.col-md-4.col-sm-3 p')
+    .click(function() {
+        // just send them to the contact form?
     });
-    $('html, body').animate(
-      {
-        scrollTop: 0
-      },
-      'slow',
-      function() {
-        // console.log('success!')
-      }
-    );
-  } else if (m !== undefined) {
-    $('#collapsedMenu.in').removeClass('in');
-    $('.responsiveHeader').css({ visibility: 'hidden', display: 'none' });
-    $('section').not(m).css({ visibility: 'hidden', display: 'none' });
-    $('section' + m).css({ display: 'block', visibility: 'visible' });
-    $('section' + m).animate(
-      {
-        scrollTop: 0
-      },
-      'slow',
-      function() {
-        // console.log('success!')
-      }
-    );
-  }
-});
 
-$('.col-xs-3.col-md-4').click(function() {
-  var productTarget = $(this).find('a').attr('href');
-  scrollToProductSection(productTarget);
-});
+$('a')
+    .click(function() {
+        var m = $(this).attr('href');
+        const regex = /["product"]\d/g;
+        let product = null;
+        product = regex.exec(m);
 
-function scrollToProductSection(selector) {
-  // console.log(selector);
-  $('.responsiveHeader').css({ visibility: 'hidden', display: 'none' });
-  $('section').not(selector).css({ visibility: 'hidden', display: 'none' });
-  $('section.products').css({
-    visibility: 'visible',
-    display: 'block'
-  });
-  $('div.product').not(selector).css({ visibility: 'hidden', display: 'none' });
-  $('section' + selector + '.container-fluid.product').css({
-    visibility: 'visible',
-    display: 'block'
-  });
-  $('#products').animate(
-    {
-      scrollTop: 0
-    },
-    'slow',
-    function() {
-      // console.log('success!')
-    }
-  );
-}
+        $('.responsiveHeader')
+                .css({ visibility: 'hidden', display: 'none' });
+        $('section')
+                .not(m)
+                .css({ visibility: 'hidden', display: 'none' });
+        $('section' + m)
+                .css({ display: 'block', visibility: 'visible' });
+
+        if (product) {
+            $('.products')
+                .css({ display: 'block', visibility: 'visible' });
+        } else if (m === "#home") {
+            $('.responsiveHeader')
+                .css({ visibility: 'visible', display: 'block' });
+          }
+    });
