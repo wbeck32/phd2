83c83883c36a90dcc8ed4102cac4293ce7c0c6f1
Wendy Beck <webeck@gmail.com>
2017-08-05 23:21:07 -0700

    displaying a modal after user submits form

--- build/js/contactForm.js
+++ build/js/contactForm.js
@@ -1,23 +1,34 @@
 let mailObject = {};
 
-$('form').on('submit', (event) => {
-  event.preventDefault();
-  mailObject = $('form').serializeArray();
+$('form')
+    .on('submit', (event) => {
+        event.preventDefault();
+        mailObject = $('form')
+            .serializeArray();
 
-  $.post({
-    url: "send.php",
-    dataType: "json",
-    data: mailObject
-  })
+        $.post({
+                url: "send.php",
+                dataType: "json",
+                data: mailObject
+            })
 
-    .done(function(res) {
-      // console.log('done: ', res);
-  })
-    .fail(function(err) {
-      // console.log('error: ', err);
-  })
-    .always(function() {
-      // console.log('finished');
+            .done(function(res) {
+                // console.log('done: ', res);
+            })
+            .fail(function(err) {
+                // console.log('error: ', err);
+            })
+            .always(function() {
+                $('form')
+                    .get(0)
+                    .reset();
+                $('#thanksModal')
+                    .modal('show');
+                $('.responsiveHeader')
+                    .css({ display: 'block' });
+                $('section')
+                    .css({ display: 'none' });
+                // console.log('finished');
+            });
+
     });
-
-  });
