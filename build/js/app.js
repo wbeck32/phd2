8e3994b91f7e2aa0a072c4b611c8e56642bf8324
Wendy Beck <webeck@gmail.com>
2017-08-05 19:08:56 -0700

    diagnosing a problem where amount of body content that appears is affected by header and footer

--- build/js/app.js
+++ build/js/app.js
@@ -7,24 +7,29 @@ $('.footerTxt.col-md-4.col-sm-3 p')
 
 $('a')
     .click(function() {
-        var m = $(this).attr('href');
+        var m = $(this)
+            .attr('href');
+        if (m === undefined) return;
+
         const regex = /["product"]\d/g;
         let product = null;
         product = regex.exec(m);
 
         $('.responsiveHeader')
-                .css({ visibility: 'hidden', display: 'none' });
+            .css({ display: 'none' });
         $('section')
-                .not(m)
-                .css({ visibility: 'hidden', display: 'none' });
+            .not(m)
+            .css({ display: 'none' });
         $('section' + m)
-                .css({ display: 'block', visibility: 'visible' });
+            .css({ display: 'block' });
+        $('.collapse')
+            .removeClass('in');
 
         if (product) {
             $('.products')
-                .css({ display: 'block', visibility: 'visible' });
+                .css({ display: 'block' });
         } else if (m === "#home") {
             $('.responsiveHeader')
-                .css({ visibility: 'visible', display: 'block' });
-          }
+                .css({ display: 'block' });
+        }
     });
